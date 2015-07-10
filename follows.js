0a7d138421644ee808c2e85f66abd78e114d220b
zvalley <ziller.public@gmail.com>
2015-07-10 15:41:14 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'MQNIDlgNPIssz4tA7f3SNJlom6JsCJEd1BUq7gO1hYCgMM5S', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [18869070], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'ABJ Zvalley Live Data', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
